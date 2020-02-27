9c7db0c639808a7cf7918ce077d3f794ef3526aa
douhoujian <371608945@qq.com>
2020-02-27 14:56:32 +0800

    Initial commit

---
+++ src/com/shunneng/java/Test.java
@@ -0,0 +1,11 @@
+package com.shunneng.java;
+
+/**
+ * @author : dhj
+ * @date : 2020/2/27 14:36
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("世界你好");
+    }
+}
